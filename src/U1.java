d80b588991de6c214bc3c99543c593f327fecd19
maciekserwin <maciekserwin@gmail.com>
2023-07-01 01:14:50 +0200

    add override method

--- src/U1.java
+++ src/U1.java
@@ -4,4 +4,14 @@ public class U1 extends Rocket {
         super(100, 10000, 18000, 0.05, 0.01, 100);
     }
 
+    @Override
+    public boolean launch() {
+        double random = Math.random();
+        return random > getChanceOfLaunchExplosion() * (getCurrentWeight() - getRocketWeight()) / (getMaxWeight() - getRocketWeight());
+    }
+
+    @Override
+    public boolean land() {
+        return super.land();
+    }
 }
