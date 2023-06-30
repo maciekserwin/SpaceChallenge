ecf6b831542cfd2b5ca068563cf26cfd0eadbc9f
maciekserwin <maciekserwin@gmail.com>
2023-07-01 01:56:14 +0200

    create in main simulation code: load fleet of U1 rockets for phase-1 and phase-2

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,9 @@ public class Main {
         ArrayList<Item> phase1Items = simulation.loadItems("phase-1.txt");
         ArrayList<Item> phase2Items = simulation.loadItems("phase-2.txt");
 
+        // Load fleet of U1 rockets for Phase-1 and Phase-2
+        ArrayList<Rocket> u1RocketsPhase1 = simulation.loadRockets(phase1Items, new U1());
+        ArrayList<Rocket> u1RocketsPhase2 = simulation.loadRockets(phase2Items, new U1());
 
     }
 }
