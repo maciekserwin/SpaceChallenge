f1d659b98d787336a28520c15e0f631bfcec6307
maciekserwin <maciekserwin@gmail.com>
2023-07-01 01:23:12 +0200

    add override methods launch and land

--- src/U2.java
+++ src/U2.java
@@ -2,4 +2,14 @@ public class U2 extends Rocket {
     public U2() {
         super(120, 18000, 29000, 0.04, 0.08, 120);
     }
+
+    @Override
+    public boolean launch() {
+        return super.launch();
+    }
+
+    @Override
+    public boolean land() {
+        return super.land();
+    }
 }
