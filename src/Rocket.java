8897e557e47b72b61c3ffb82194b6e10b90e7bf7
maciekserwin <maciekserwin@gmail.com>
2023-07-01 00:02:17 +0200

    add Rocket class with field int: rocketCost, rocketWeight, maxWeight, currentWeight and double: chanceOfLaunchExplosion, chanceOfLandingCrash

---
+++ src/Rocket.java
@@ -0,0 +1,10 @@
+public class Rocket {
+
+   private int rocketCost;
+   private int rocketWeight;
+   private int maxWeight;
+   private double chanceOfLaunchExplosion;
+   private double chanceOfLandingCrash;
+   private int currentWeight;
+
+}
